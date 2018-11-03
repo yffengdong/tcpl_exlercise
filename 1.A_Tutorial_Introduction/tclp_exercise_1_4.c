82d100a9b09fd0e7548082bd5bc4ae5dad1ccbb8
yunfeng <yffengdong@gmail.com>
2018-11-03 19:07:20 +0800

    del others ';'

--- 1.A_Tutorial_Introduction/tclp_exercise_1_4.c
+++ 1.A_Tutorial_Introduction/tclp_exercise_1_4.c
@@ -6,6 +6,6 @@ int main()
     //float celsius = -17.8f;
     printf("celsius\t fahr\n");
     for(float celsius = -17.8f; celsius < 300; celsius += 11.1f)
-        printf("%5.1f\t%6.1f\n", celsius, (celsius / (5.0/9.0) + 32.0));;
+        printf("%5.1f\t%6.1f\n", celsius, (celsius / (5.0/9.0) + 32.0));
     return 0;
 }
