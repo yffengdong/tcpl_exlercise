64e7b11ff6f436cb7d6fa1f5b37effe91d94f15b
yunfeng <yffengdong@gmail.com>
2018-11-21 15:45:39 +0800

    Create tclp_exercise_1_17.c

---
+++ 1.A_Tutorial_Introduction/tclp_exercise_1_17.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#define MAXLINE 1000
+
+int getline(char line[], int maxline);
+
+int main(int argc, char const *argv[])
+{
+    int len;
+    char line[MAXLINE];
+    while((len=getline(line, MAXLINE))>0)
+        if(len > 80)
+        {
+            printf("%s", line);
+        }
+    return 0;
+}
+
+int getline(char s[], int lim)
+{
+    int i, c;
+    for(i = 0; i<lim - 1 && (c= getchar())!=EOF && c!='\n';++i)
+        s[i] = c;
+    if(c == '\n')
+    {
+        s[i] = c;
+        ++i;
+    s[i] = '\0';
+    return i;
+    }
+}
+
+// void copy(char to[], char from[])
+// {
+//     int i;
+//     i = 0;
+//     while((to[i] = from[i]) != '\0')
+//         ++i;
+// }
